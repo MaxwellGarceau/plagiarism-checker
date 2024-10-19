a965e81cfc2a5dd539f84a87fc88a7299566325b
Max Garceau <maxwellgarceau@gmail.com>
2024-10-19 15:30:28 -0600

    fix: Fix bug causing failure in enforcing required properties

--- src/Includes/Admin_Ajax.php
+++ src/Includes/Admin_Ajax.php
@@ -7,15 +7,19 @@ namespace Max_Garceau\Plagiarism_Checker\Includes;
 use Max_Garceau\Plagiarism_Checker\Services\Nonce_Service;
 use Max_Garceau\Plagiarism_Checker\Services\Nonce_Status;
 use Max_Garceau\Plagiarism_Checker\Includes\Api_Client;
+use Monolog\Logger;
 
 class Admin_Ajax {
 
 	/**
 	 * @param Nonce_Service $nonce_service
+	 * @param Api_Client    $api_client
+	 * @param Logger        $logger
 	 */
 	public function __construct(
 		private readonly Nonce_Service $nonce_service,
-		private readonly Api_Client $api_client
+		private readonly Api_Client $api_client,
+		private readonly Logger $logger
 	) {}
 
 	public function handle_plagiarism_checker_request(): void {
@@ -42,32 +46,40 @@ class Admin_Ajax {
 		}
 
 		// Enforce that we have the properties our app requires
-		$data = $this->enforce_required_properties( $data );
-		if ( is_wp_error( $data ) ) {
-			// TODO: Log failure here
+		if ( ! $this->response_has_required_properties( $data ) ) {
+			$this->logger->error( 'API request failed. The response is missing required properties.' );
 
 			// The request was processed correctly, but there was a discrepancy
 			// in the contract between the client and server.
 			wp_send_json_error( 'API request failed.', 422 );
 		}
 
+		$this->logger->info(
+			'API request successful. Returning the data to the frontend.',
+			$data
+		);
+
 		// Process the response and send it back to the frontend
 		wp_send_json_success( $data );
 	}
 
 	/**
-	 * Ensure that the required properties for our API response are present
+	 * Validate that the required properties are present in each result object
+	 * in the API response.
 	 *
 	 * TODO: This is probably better off in another class, but let's revisit
 	 * that idea in the future when the app is bigger.
 	 *
 	 * TODO: I'd like to use Symfony's Validator class here, but I'd prefer to
 	 * keep the dependencies minimal. However, let's revisit this in the future.
+	 *
+	 * @param array $data The actual response data to validate (an array of results).
+	 * @return bool True if all result objects have the required properties, false otherwise.
 	 */
-	private function enforce_required_properties( array $data ): bool {
-		// Ensure required properties are present
+	private function response_has_required_properties( array $data ): bool {
+		// Define the required properties structure for each result object
 		$required_properties = array(
-			'id',
+			'url',
 			'title',
 			'primary_artist' => array(
 				'name',
@@ -75,29 +87,10 @@ class Admin_Ajax {
 			),
 		);
 
-		$actual_properties = $this->array_keys_recursive( $data );
-
-		// Ensure that all properties from $required_properties are present in $actual_properties
-		return $this->check_required_properties( $required_properties, $actual_properties );
-	}
-
-	private function check_required_properties( array $required_properties, array $data ): bool {
-		foreach ( $required_properties as $key => $value ) {
-			if ( is_array( $value ) ) {
-				// If the value is an array, the corresponding key in $data must also be an array
-				if ( ! isset( $data[ $key ] ) || ! is_array( $data[ $key ] ) ) {
-					return false;
-				}
-
-				// Recursively check the nested properties
-				if ( ! $this->check_required_properties( $value, $data[ $key ] ) ) {
-					return false;
-				}
-			} else {
-				// Check if the simple key exists
-				if ( ! array_key_exists( $value, $data ) ) {
-					return false;
-				}
+		// Loop over each result object and validate its properties
+		foreach ( $data as $resource ) {
+			if ( ! $this->check_required_properties( $required_properties, $resource['result'] ) ) {
+				return false;
 			}
 		}
 
@@ -105,25 +98,32 @@ class Admin_Ajax {
 	}
 
 	/**
-	 * Loop through an array and return all keys as the same array structure
+	 * Check if all required properties exist in a single result object.
 	 *
-	 * TODO: Not sure where to put this. Leaving here for now since it's a helper
-	 * that we only need in this class.
-	 *
-	 * Answer in php.net
-	 * https://www.php.net/manual/en/function.array-keys.php#114584
+	 * @param array $required_properties The required properties structure.
+	 * @param array $data The result object from the API.
+	 * @return bool True if all required properties are present, false otherwise.
 	 */
-	private function array_keys_recursive( array $my_array, int $MAXDEPTH = PHP_INT_MAX, int $depth = 0, array $array_keys = array() ): array {
-		if ( $depth < $MAXDEPTH ) {
-			++$depth;
-			$keys = array_keys( $my_array );
-			foreach ( $keys as $key ) {
-				if ( is_array( $my_array[ $key ] ) ) {
-					$array_keys[ $key ] = $this->array_keys_recursive( $my_array[ $key ], $MAXDEPTH, $depth );
+	private function check_required_properties( array $required_properties, array $data ): bool {
+		foreach ( $required_properties as $key => $value ) {
+			if ( is_array( $value ) ) {
+				// If value is an array, check that the corresponding key exists and is an array
+				if ( ! isset( $data[ $key ] ) || ! is_array( $data[ $key ] ) ) {
+					return false;
+				}
+
+				// Recursively check nested properties
+				if ( ! $this->check_required_properties( $value, $data[ $key ] ) ) {
+					return false;
+				}
+			} else {
+				// Check if the key exists in the data
+				if ( ! array_key_exists( $value, $data ) ) {
+					return false;
 				}
 			}
 		}
 
-		return $array_keys;
+		return true;
 	}
 }
