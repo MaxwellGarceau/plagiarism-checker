ab2c007c06c9d2bb6b4524bd546c9f83ca8a7ebf
Max Garceau <maxwellgarceau@gmail.com>
2024-10-20 12:32:34 -0600

    fix: Fix bug where we were not updating /wp/src tests config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -43,22 +43,14 @@ if ( isset( $GLOBALS['argv'] ) && isset( $GLOBALS['argv'][1] ) && strpos( $GLOBA
  */
 if ( isset( $GLOBALS['argv'] ) && isset( $GLOBALS['argv'][1] ) && strpos( $GLOBALS['argv'][1], 'integration' ) !== false ) {
 
-	if ( ! file_exists( dirname( __DIR__, 1 ) . '/wp/tests/phpunit/wp-tests-config.php' ) ) {
-		// We need to set up core config details and test details
-		copy( dirname( __DIR__, 1 ) . '/wp/wp-tests-config-sample.php', dirname( __DIR__, 1 ) . '/wp/tests/phpunit/wp-tests-config.php' );
-
-		// Change certain constants from the test's config file.
-		$testConfigPath     = dirname( __DIR__, 1 ) . '/wp/tests/phpunit/wp-tests-config.php';
-		$testConfigContents = file_get_contents( $testConfigPath );
-
-		$testConfigContents = str_replace( "dirname( __FILE__ ) . '/src/'", "dirname(__FILE__, 3) . '/src/'", $testConfigContents );
-		$testConfigContents = str_replace( 'youremptytestdbnamehere', $_SERVER['DB_NAME'], $testConfigContents );
-		$testConfigContents = str_replace( 'yourusernamehere', $_SERVER['DB_USER'], $testConfigContents );
-		$testConfigContents = str_replace( 'yourpasswordhere', $_SERVER['DB_PASSWORD'], $testConfigContents );
-		$testConfigContents = str_replace( 'localhost', $_SERVER['DB_HOST'], $testConfigContents );
-
-		file_put_contents( $testConfigPath, $testConfigContents );
-	}
+	/**
+	 * Copy and overwrite the /wp/tests/phpunit/wp-tests-config.php file every time
+	 * It's worth it to avoid the troubleshooting that will inevitably come from a stale config file.
+	 */
+	// if ( ! file_exists( dirname( __DIR__, 1 ) . '/wp/tests/phpunit/wp-tests-config.php' ) ) {
+		require_once dirname( __DIR__, 1 ) . '/tests/__bootstrap/WpTestConfigManager.php';
+		(new WpTestConfigManager($_SERVER))->init();
+	// }
 
 	// Give access to tests_add_filter() function.
 	require_once dirname( __DIR__, 1 ) . '/wp/tests/phpunit/includes/functions.php';
