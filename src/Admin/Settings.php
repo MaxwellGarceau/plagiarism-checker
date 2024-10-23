afedd968aa0010c5a60bc03585b4fc2ac1dd2396
Max Garceau <maxwellgarceau@gmail.com>
2024-10-23 17:53:34 -0600

    refactor: convert to tabs

--- src/Admin/Settings.php
+++ src/Admin/Settings.php
@@ -20,47 +20,47 @@ use Max_Garceau\Plagiarism_Checker\Admin\Token_Storage;
  */
 class Settings {
 
-    const SETTINGS_SECTION_ID = 'plagiarism_checker_settings_section';
-    const NONCE_ACTION = 'plagiarism_checker_save_token';
-    const NONCE_NAME = 'plagiarism_checker_nonce';
+	const SETTINGS_SECTION_ID = 'plagiarism_checker_settings_section';
+	const NONCE_ACTION = 'plagiarism_checker_save_token';
+	const NONCE_NAME = 'plagiarism_checker_nonce';
 
-    private Menu_Constants $constants;
-    private Token_Storage $token_storage;
+	private Menu_Constants $constants;
+	private Token_Storage $token_storage;
 
-    public function __construct( Menu_Constants $constants, Token_Storage $token_storage ) {
-        $this->constants     = $constants;
-        $this->token_storage = $token_storage;
-    }
+	public function __construct( Menu_Constants $constants, Token_Storage $token_storage ) {
+		$this->constants     = $constants;
+		$this->token_storage = $token_storage;
+	}
 
-    public function plagiarism_checker_settings_page(): void {
-        ?>
-        <div class="wrap">
-            <h1><?php esc_html_e( 'Plagiarism Checker Settings', $this->constants->get_menu_slug() ); ?></h1>
+	public function plagiarism_checker_settings_page(): void {
+		?>
+		<div class="wrap">
+			<h1><?php esc_html_e( 'Plagiarism Checker Settings', $this->constants->get_menu_slug() ); ?></h1>
 			<?php $this->plagiarism_checker_settings_section_callback() ?>
-            <form method="post" action="admin-post.php">
-                <input type="hidden" name="action" value="save_plagiarism_checker_token">
-                <?php
-                wp_nonce_field( self::NONCE_ACTION, self::NONCE_NAME );
-                $this->render_api_token_field();
-                submit_button();
-                ?>
-            </form>
-        </div>
-        <?php
-    }
+			<form method="post" action="admin-post.php">
+				<input type="hidden" name="action" value="save_plagiarism_checker_token">
+				<?php
+				wp_nonce_field( self::NONCE_ACTION, self::NONCE_NAME );
+				$this->render_api_token_field();
+				submit_button();
+				?>
+			</form>
+		</div>
+		<?php
+	}
 
 	/**
 	 * Callback for the settings section.
 	 * 
 	 * Load the HTML from an html file.
 	 */
-    public function plagiarism_checker_settings_section_callback(): void {
+	public function plagiarism_checker_settings_section_callback(): void {
 		require plugin_dir_path( __DIR__ ) . 'Admin/Views/api_instructions.html';
-    }
+	}
 
-    private function render_api_token_field(): void {
-        $user_id = get_current_user_id();
-        $token   = $this->token_storage->get_token( $user_id );
-        echo '<input type="text" name="plagiarism_checker_api_token" value="' . esc_attr( $token ) . '" class="regular-text">';
-    }
+	private function render_api_token_field(): void {
+		$user_id = get_current_user_id();
+		$token   = $this->token_storage->get_token( $user_id );
+		echo '<input type="text" name="plagiarism_checker_api_token" value="' . esc_attr( $token ) . '" class="regular-text">';
+	}
 }
