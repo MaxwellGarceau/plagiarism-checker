b51e569efa0a686c27040acfbbd365e407962f4e
Max Garceau <maxwellgarceau@gmail.com>
2024-10-24 21:08:05 -0600

    feat: Add example URL to API token instructions

--- src/Admin/Views/api_instructions.php
+++ src/Admin/Views/api_instructions.php
@@ -3,7 +3,7 @@
 <h2>How to generate an API token:</h2>
 <p>1. Go to Genius.com and <a href="https://genius.com/signup" target="_blank">create an account</a>.</p>
 <p>2. Visit the <a href="https://genius.com/api-clients/new" target="_blank">API section</a> in your account settings.</p>
-<p>3. Fill in the fields and generate a new API token.</p>
+<p>3. Fill in the fields and generate a new API token. <span class="plagiarism-checker__highlight">Use <code style="display: inline-block">https://example.com/</code> for your App Website URL.</span></p>
 <p>4. Copy and paste the "Client Secret" into the field below.</p>
 <p class="plagiarism-checker__note">More information on the Genius.com API can be found in the <a href="https://docs.genius.com/#/authentication-h1" target="_blank">developer documentation</a>.</p>
 <?php
