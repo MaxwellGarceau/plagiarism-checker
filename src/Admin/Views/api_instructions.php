061449ff6e672bf626bdb9d1107b8a9e5b09d508
Max Garceau <maxwellgarceau@gmail.com>
2024-10-25 18:49:12 -0600

    feat: Update api token instructions for clear use

--- src/Admin/Views/api_instructions.php
+++ src/Admin/Views/api_instructions.php
@@ -1,22 +1,48 @@
-<p>Enter your Genius.com API token below. This is required for the Plagiarism Checker to function.</p>
-<p>Instructions for generating an API token can be found below:</p>
-<h2>How to generate an API token:</h2>
-<p>1. Go to Genius.com and <a href="https://genius.com/signup" target="_blank">create an account</a>.</p>
-<p>2. Visit the <a href="https://genius.com/api-clients/new" target="_blank">API section</a> in your account settings.</p>
-<p>3. Fill in the fields and generate a new API token. <span class="plagiarism-checker__highlight">Use <code style="display: inline-block">https://example.com/</code> for your App Website URL.</span></p>
-<p>4. Copy and paste the "Client Secret" into the field below.</p>
-<p class="plagiarism-checker__note">More information on the Genius.com API can be found in the <a href="https://docs.genius.com/#/authentication-h1" target="_blank">developer documentation</a>.</p>
 <?php
-if ( isset( $_GET['status'] ) && $_GET['status'] === 'error' ) {
-	?>
-		<p class="plagiarism-checker__status plagiarism-checker__error" style="color:#dc3232"><strong>Error:</strong> There was an error saving your token. Please try again.</p>
-		<p class="plagiarism-checker__note"><strong>Note:</strong> If you need to regenerate your token, you can do so in the <a href="https://genius.com/api-clients/new" target="_blank">API section</a> of your Genius.com account settings.</p>
-	<?php
-}
+/**
+ * View for the API instructions page.
+ *
+ * @package PlagiarismChecker\Admin\Views
+ */
 
-if ( isset( $_GET['status'] ) && $_GET['status'] === 'success' ) {
-	?>
-		<p class="plagiarism-checker__status plagiarism-checker__success" style="color:#00a0d2"><strong>Success:</strong> Your token has been saved.</p>
-	<?php
+/**
+ * Get the URL for the admin images directory.
+ */
+function get_admin_img_dir_url( $img = '' ) {
+	return plugin_dir_url( __DIR__ ) . 'assets/imgs/' . $img;
 }
 ?>
+<section class="plagiarism-checker__api-instructions-container">
+	<p>Enter your Genius.com API token below. This is required for the Plagiarism Checker to function.</p>
+	<p>Instructions for generating an API token can be found below:</p>
+	<h2>How to generate an API token:</h2>
+	<ol class="plagiarism-checker__list">
+		<li>Go to Genius.com and <a href="https://genius.com/signup" target="_blank">create an account</a>.</li>
+		<li>Visit the <a href="https://genius.com/api-clients/new" target="_blank">API section</a> in your account settings.</li>
+		<li>
+			Fill in the "App Name" and "App Website URL" fields. <span class="plagiarism-checker__highlight">Use <code style="display: inline-block">https://example.com/</code> for your App Website URL.</span> Then click "save".
+			<img class="plagiarism-checker__img" src="<?php echo get_admin_img_dir_url( 'new-api-client.png' ); ?>"></img>
+		</li>
+		<li>Find the section that says <span class="plagiarism-checker__highlight">"Client Access Token"</span> and click the button that says <span class="plagiarism-checker__highlight">"Generate Access Token"</span>.
+			<img class="plagiarism-checker__img" src="<?php echo get_admin_img_dir_url( 'generate-access-token.png' ); ?>"></img>
+		</li>
+		<li>Copy and paste the <span class="plagiarism-checker__highlight">"Client Access Token"</span> into the field below.
+			<img class="plagiarism-checker__img" src="<?php echo get_admin_img_dir_url( 'copy-access-token.png' ); ?>"></img>
+		</li>
+	</ol>
+	<p class="plagiarism-checker__note">Note: Make sure you copy/paste the <span class="plagiarism-checker__highlight">"Client Access Token"</span> token and NOT the "Client Secret". More information on the Genius.com API can be found in the <a href="https://docs.genius.com/#/authentication-h1" target="_blank">developer documentation</a>.</p>
+	<?php
+	if ( isset( $_GET['status'] ) && $_GET['status'] === 'error' ) {
+		?>
+			<p class="plagiarism-checker__status plagiarism-checker__error" style="color:#dc3232"><strong>Error:</strong> There was an error saving your token. Please try again.</p>
+			<p class="plagiarism-checker__note"><strong>Note:</strong> If you need to regenerate your token, you can do so in the <a href="https://genius.com/api-clients/new" target="_blank">API section</a> of your Genius.com account settings.</p>
+		<?php
+	}
+
+	if ( isset( $_GET['status'] ) && $_GET['status'] === 'success' ) {
+		?>
+			<p class="plagiarism-checker__status plagiarism-checker__success" style="color:#00a0d2"><strong>Success:</strong> Your token has been saved.</p>
+		<?php
+	}
+	?>
+<section>
